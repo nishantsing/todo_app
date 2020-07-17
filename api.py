67720484f80a9d575dbc33a081446ec93ae5fa7c
Xarvis <nsnishant51@gmail.com>
2020-07-17 21:45:57 +0530

    login

--- api.py
+++ api.py
@@ -1,26 +1,24 @@
 from firebase import firebase
+from flask_cors import CORS, cross_origin
 import flask
 from flask import request, jsonify, render_template, redirect, url_for
-import pymongo
-
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-db = client['tododb']
-collection = db['mycollection']
-
 
 firebase = firebase.FirebaseApplication("https://todo-8a912.firebaseio.com/", None)
 
-
-
 app = flask.Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
 def displayAll():
     return firebase.get('/collection',None)
 
 @app.route('/', methods=['GET'])
+def getLogin():
+    return render_template("login.html")     #template inside templates
+
+@app.route('/home', methods=['GET'])
 def getHome():
-    return render_template("home.html")     #template inside templates
+    return render_template("home.html")     #template inside templates    
 
 
 @app.route('/display-all', methods=['GET'])
@@ -31,9 +29,11 @@ def getDisplayAll():
 
 @app.route('/add',methods=['POST','GET'])
 def getAdd():
-    if request.method == 'GET':
-        taskname = request.form.get('taskname', type=str)
-        entryname = request.form.get('entryname', type=str)
+    if request.method == 'POST':
+        data = request.json
+        return jsonify(data)
+        # taskname = request.form.get('taskname', type=str)
+        # entryname = request.form.get('entryname', type=str)
         # user = 'user1'
 
         data = [{'entryname': entryname}]
