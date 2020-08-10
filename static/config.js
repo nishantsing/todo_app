00332e5c049410d337d0fd7dd1c8067ddf8278d2
Yash Karanje <yashkaranje98@gmail.com>
2020-08-10 17:49:20 +0530

    gitignore hataya

---
+++ static/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBzz5pfaGt47zZ3zDWMffz-WsRV_jpFPAc",
+    authDomain: "todo-8a912.firebaseapp.com",
+    databaseURL: "https://todo-8a912.firebaseio.com",
+    projectId: "todo-8a912",
+    storageBucket: "todo-8a912.appspot.com",
+    messagingSenderId: "544642646725",
+    appId: "1:544642646725:web:74df1e11dc28e21a3de6ac",
+    measurementId: "G-ZQK0L0QCZ8"
+  };
